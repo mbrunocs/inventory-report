99aa2bde0434c8a3cc903d3cffa479e2214252b6
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-09-26 12:15:16 -0300

    dev: test2 whthlldng

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,14 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    cadeira = Product(
+        10,
+        'Cadeira gamer',
+        'GamerCompany',
+        '2020',
+        '2022',
+        845120956230,
+        'Longe de umidade')
+
+    assert cadeira.__repr__().__contains__('GamerCompany')
